c98591c1bd60896fdde22751d0351663bbc407c0
Saba Al hrishat <saba.alhrishat2002@gmail.com>
2025-01-17 13:52:33 +0300

    well

--- t1.js
+++ t1.js
@@ -35,3 +35,73 @@ const even = number
 .filter(ele => ele % 2 === 0 )
 .reduce(( sum , cur) => sum +cur ,0 );
 console.log(even);
+
+// t6
+
+const word = ["apple", "banana" , "cherry" , "date"];
+const re = word.map(function(ele) {
+    return ele === ele.toUpperCase() ? ele.toLowerCase() : ele.toUpperCase();
+}).join(" ");
+console.log(re);
+
+// t7
+
+const heros = [
+    {name: 'Iron Man', power: 'Tech'},
+    {name: 'Spider-Man', power: 'Spider abilities'},
+    {name: 'Thor', power: 'Godly powers'},
+    {name: 'Hulk', power: 'Super strength'}
+  ];
+  
+  const newHeros = heros.map((hero, index) => {
+    return {
+      hero: hero.name, 
+      power: hero.power,
+      id: index 
+    };
+  });
+  
+  console.log(newHeros);
+  
+
+
+//   t8
+
+const inputWords = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+const our = inputWords.filter(function (ele){
+
+return ele.length >=7;
+
+})
+console.log(our);
+
+// t9
+
+const nu = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const odd = nu.filter(num => num %2 !==0);
+console.log(odd);
+
+const div = nu.filter(num => num %2 !==0 || num %5 ===0 );
+console.log(div);
+
+const divseq = nu.filter(num => num %3 !==0).map(num => num ** 2);
+console.log(divseq);
+
+const sq = nu.filter(num => num %5 ===0 )
+
+.map( num => num ** 2 )
+.reduce ((sum, num ) => sum , num ,0 );
+
+console.log(sq);
+
+// t10
+
+let nums = [11, 22, 33, 46, 75, 86, 97, 98];
+let squaredEvenNums = nums
+.filter(num => num % 2 === 0) 
+  .map(num => num ** 2);
+
+console.log (`squaredEvenNums: ${squaredEvenNums}`); 
+
+let sum = nums.reduce((total, num) => total + num, 0); 
+console.log (`Sum of array elements: ${sum}`); 
